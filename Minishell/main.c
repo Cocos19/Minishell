7c34d036b45bbf1653c36d53732bc5b91052fda1
Profetto Marco <profetto.marco@gmail.com>
2023-07-04 14:30:54 +0200

    update

--- Minishell/main.c
+++ Minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: mprofett <mprofett@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 10:02:04 by mprofett          #+#    #+#             */
-/*   Updated: 2023/07/04 08:29:47 by mprofett         ###   ########.fr       */
+/*   Updated: 2023/07/04 14:03:30 by mprofett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,8 +63,8 @@ void	read_prompt(t_shell *shell)
 	if (user_input && input_is_valid(shell) == 0)
 	{
 		lexer(shell, user_input);
-		parser(shell);
 		activate_signals(IGNORE_MODE);
+		parser(shell);
 		if (shell_has_to_be_exited(shell) == 0)
 			single_cmd_builtin_exit(shell, shell->pipe_lst);
 		else if (shell->pipe_lst)
